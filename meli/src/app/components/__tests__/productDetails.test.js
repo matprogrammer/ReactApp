cc155b18f82f80566ae93e8dad501d9249b13dc3
Mauro <matprogrammer@gmail.com>
2020-09-28 20:02:14 -0300

    test product details

--- meli/src/app/components/__tests__/productDetails.test.js
+++ meli/src/app/components/__tests__/productDetails.test.js
@@ -6,19 +6,23 @@ import { shallow, configure } from 'enzyme';
 configure({adapter: new Adapter()});
 
 function getProps() {
-  const product = [{
-    "title": "iPhone XS 512 Gb Plata 4 Gb Ram",
-      "price": {
-          "currency": "ARS",
-          "amount": "193989",
-          "decimals": "99"
-      },
-      "picture": "http://http2.mlstatic.com/D_848011-MLA31002678112_062019-I.jpg",
-      "condition": "new",
-      "free_shipping": true,
-      "sold_quantity": 0,
-      "description": "iPhone XS 512 Gb Plata 4 Gb Ram"
-  }]
+  const product = {
+    "product": [
+      {
+        "title": "iPhone XS 512 Gb Plata 4 Gb Ram",
+          "price": {
+              "currency": "ARS",
+              "amount": "193989",
+              "decimals": "99"
+          },
+          "picture": "http://http2.mlstatic.com/D_848011-MLA31002678112_062019-I.jpg",
+          "condition": "new",
+          "free_shipping": true,
+          "sold_quantity": 0,
+          "description": "iPhone XS 512 Gb Plata 4 Gb Ram"
+      }
+    ]
+  }
   return product;
 }
 
